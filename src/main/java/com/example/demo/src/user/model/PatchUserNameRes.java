480bd11c10745dcbfefb34a8af03f743ab76ec98
sweetchinmusic <itsbigpicture22@gmail.com>
2021-06-19 06:48:55 +0900

    4nd

--- src/main/java/com/example/demo/src/user/model/PatchUserNameRes.java
+++ src/main/java/com/example/demo/src/user/model/PatchUserNameRes.java
@@ -1,6 +1,5 @@
 package com.example.demo.src.user.model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +7,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-public class GetUserNameRes {
+public class PatchUserNameRes {
+
     private int userId;
     private String userName;
-
 }
